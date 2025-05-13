ddd748568aeac860193c486745bcb698d3cf9064
Vitor Hugo Oliveira <vitorh.magnus.oliveira@gmail.com>
2025-05-13 00:31:46 -0300

    create bropdox folder

--- server/src/ServerFileManager.cpp
+++ server/src/ServerFileManager.cpp
@@ -1,9 +1,20 @@
 #include <ServerFileManager.hpp>
 
 void ServerFileManager::create_sync_dir(std::string username) {
-    std::string folder_name = "./server/bropdox/sync_dir_" + username;
+    int status;
+    
+    // cria pasta do bropdox
+    std::string folder_name = "./server/bropdox";
+    status = mkdir(folder_name.c_str(), 0777);
+    if(status !=0) {
+        if (errno != EEXIST) {
+             std::cerr << "[ERRO] Falha ao criar pasta " << std::endl;
+        } 
+    }
 
-    int status = mkdir(folder_name.c_str(), 0777);
+    // cria sync_dir do usuário
+    folder_name += "/sync_dir_" + username;
+    status = mkdir(folder_name.c_str(), 0777);
     if(status !=0) {
         if (errno != EEXIST) {
              std::cerr << "[ERRO] Falha ao criar pasta " << std::endl;
