76171dfcd92cc90cde167e0f45185d8834c4cb47
Vitor Hugo Magnus Oliveira <vitorh.magnus.oliveira@gmail.com>
2025-06-19 18:39:21 -0300

    refactor: create user syncdir on BetaServer when new client is received

--- server/src/BetaServer.cpp
+++ server/src/BetaServer.cpp
@@ -98,19 +98,18 @@ void BetaServer::handle_client_updates(Packet meta_packet) {
 
 void BetaServer::handle_client_upload(const std::string filename, const std::string username, uint32_t total_packets) {
     std::cout << "[" << username << "]" << "handle_client_upload: " << filename << std::endl;
-    FileManager::create_directory(backup_dir_path / username);
     Packet::receive_file(alfa_socket_fd, filename, backup_dir_path / username, total_packets);
 }
 
 void BetaServer::handle_client_delete(const std::string filename, const std::string username) {
     std::cout << "[ ALFA THREAD ] " << "[" << username << "] " << "handle_client_delete: " << filename << std::endl;
-    FileManager::create_directory(backup_dir_path / username);
     FileManager::delete_file(backup_dir_path / username / filename);
 }
 
 void BetaServer::handle_new_clients(const std::string ip_first_client, const std::string username_first_client, int total_clients) {
     std::cout << "[ ALFA THREAD ] " << "Added new client device" << std::endl;
     devices->add_client(username_first_client, -1, ip_first_client);
+    FileManager::create_directory(backup_dir_path / username_first_client);
     while(--total_clients > 0) {
         Packet username_packet = Packet::receive(alfa_socket_fd);
         if (username_packet.type != static_cast<uint16_t>(Packet::Type::USERNAME)) {
@@ -124,6 +123,7 @@ void BetaServer::handle_new_clients(const std::string ip_first_client, const std
         }
         std::cout << "[ ALFA THREAD ] " << "Added new client device" << std::endl;
         devices->add_client(username_packet.payload, -1, ip_packet.payload);
+        FileManager::create_directory(backup_dir_path / username_packet.payload);
     }
 }
 
